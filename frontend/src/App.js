f55b51e39b8000b731c74b3ca94c141275638788
lefahla <lefahla@gmail.com>
2023-02-08 05:02:37 +0200

    Adding Not found page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Redirect, Routes } from 'react-router-dom'; 
+import {NavBar, Cart, Home, NotFound } from './components'
 
 function App() {
   return (
-    <>
-      thabo
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Routes>        
+          <Route path='/cart' compoenent={<Cart />} /> 
+          <Route path='/' exact component={<Home /> } />
+          <Redirect to='/not-found' />
+        </Routes>
+      </BrowserRouter> 
+    </div>
   );
 }
 
