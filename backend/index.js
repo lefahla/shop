354b2bfbce0b0322849a845646a73ce73793b116
lefahla <lefahla@gmail.com>
2023-02-13 20:36:43 +0200

    Adding API functionality

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const product  = require("./products");
+const dotenv = require("dotenv").config();
  
 const PORT = process.env.PORT || 5000;
-const app = express();
-
+const app = express();  
+            
 app.use(express.json()) // Configuring middleware functionality,
 app.use(cors()); // This will allow us to access the api from the react application since its in another domain
 
@@ -11,6 +13,6 @@ app.get("/",(req,res) => {
     res.send("app sent") 
 });
 app.get("/cart",(req,res) => {
-    res.send([2,3,32]); 
+    res.send(product);    
 });
 app.listen(PORT,console.log(`Connected of port ${PORT}`));
---
+++ backend/products.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        "id":1,
+        "desc": "A simple phone",
+        "type": "phone",
+        "price": 566,
+        "image": "./images/galaxyS.png"
+    },{
+        "id":2,
+        "desc": "A simple person",
+        "type": "person",
+        "price": 566,
+        "image": "./images/galaxyS.png"
+    },{
+        "id":3,
+        "desc": "A simple shoe",
+        "type": "show",
+        "price": 566,
+        "image": "./images/iphone12pro.jpg"
+    },{
+        "id":4,
+        "desc": "Phone 4",
+        "type": "game",
+        "price": 566,
+        "image": "./images/iphone12.jpg"
+    },
+];
+
+module.exports = products;
